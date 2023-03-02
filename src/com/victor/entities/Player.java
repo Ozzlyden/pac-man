5aa86239a9667762e2049864d9b65f22b70b8813
Victor Valente <Valente-victor@hotmail.com>
2023-03-02 14:43:06 -0300

    Animacao do Player

--- src/com/victor/entities/Player.java
+++ src/com/victor/entities/Player.java
@@ -10,12 +10,17 @@ import com.victor.world.World;
 public class Player extends Entity {
 	
 	public boolean  stay, right, up, left, down; 
-	
-	public BufferedImage sprite_left, sprite_right, sprite_up, sprite_down;
+
+	public BufferedImage[] sprite_right;
+	private BufferedImage[] sprite_left;
 	
 	public int lastDir = 1, lastDir_up_down = 1;
 	
 	public double life = 100, maxLife = 100;
+	
+	private int frames = 0, maxFrames = 8, index = 0, maxIndex = 2;
+	private boolean moved = false;
+
 	
 	public boolean isDamaged = false;
 	private int damageFrames = 0;
@@ -23,30 +28,51 @@ public class Player extends Entity {
 	public Player(int x, int y, int width, int height,double speed, BufferedImage sprite) {
 		super(x, y, width, height,speed, sprite);
 		
-		sprite_left = Game.spritesheet.getSprite(32, 16, 16, 16); 
-		sprite_right = Game.spritesheet.getSprite(32, 0, 16, 16); 
-		sprite_up = Game.spritesheet.getSprite(32, 48, 16, 16); 
-		sprite_down = Game.spritesheet.getSprite(32, 32, 16, 16); 
+		sprite_right = new BufferedImage [4];
+		sprite_left = new BufferedImage [4];
+		
+		//sprite_right[0] = Game.spritesheet.getSprite(32, 0, 16, 16); 
+		//sprite_right[1] = Game.spritesheet.getSprite(48, 0, 16, 16); 
+		//sprite_right[2] = Game.spritesheet.getSprite(64, 0, 16, 16);
+		//sprite_right[3] = Game.spritesheet.getSprite(80, 0, 16, 16);
+
+		//sprite_left[0] = Game.spritesheet.getSprite(32, 16, 16, 16);
+		//sprite_left[1] = Game.spritesheet.getSprite(48, 16, 16, 16);
+		//sprite_left[2] = Game.spritesheet.getSprite(64, 16, 16, 16);
+		//sprite_left[3] = Game.spritesheet.getSprite(80, 16, 16, 16);
+		
+		for (int i = 0; i < 3; i++) {
+			sprite_right[i] = Game.spritesheet.getSprite(32 + (i*16), 0, 16, 16);	
+		}
+		for (int i = 0; i < 3; i++) {
+			sprite_left[i] = Game.spritesheet.getSprite(32 + (i*16), 16, 16, 16);
+		}
+
 	}
 	
 	public void tick() {
 		// CAMADA DE RENDER
 		depth = 2;
+		moved = false;
 		
 		//LOGICA DE MOVIMENTACAO
 		if(right && World.isFree((int) (x + speed), this.getY())) {
+			moved = true;
 			x+=speed;
 			lastDir = 1;
 		}
 		else if(left && World.isFree((int) (x - speed), this.getY())) {
+			moved = true;
 			x-=speed;
 			lastDir = -1;
 		}
 		if(up && World.isFree(this.getX(), (int) (y - speed))) {
+			//moved = true;
 			y-=speed;
 			lastDir_up_down = 1;
 		}	
 		else if(down && World.isFree(this.getX(), (int) (y + speed))) {
+			//moved = true;
 			y+=speed;
 			lastDir_up_down = -1;
 
@@ -55,6 +81,17 @@ public class Player extends Entity {
 		
 		if(Game.moedas_cont == Game.moedas_atual) {
 			//System.out.println("Ganhamos o jogo !");
+		}
+		
+		if(moved) {		//se o player mexer, acada 5 frames, faz uma animacao
+			frames++;
+			if(frames == maxFrames) {
+				frames = 0;
+				index++;
+				if(index > maxIndex)
+					index = 0;
+			}
+			
 		}
 	}
 	
@@ -72,20 +109,13 @@ public class Player extends Entity {
 	}
 	
 	public void render(Graphics g) {
-		if(lastDir == 1) {
-			super.render(g);
-			g.drawImage(sprite_right, this.getX() - Camera.x,this.getY() - Camera.y,this.getWidth(),this.getHeight(), null);
+		super.render(g);
+		if(moved == true && lastDir == 1) {
+			g.drawImage(sprite_right[index], this.getX() - Camera.x,this.getY() - Camera.y,this.getWidth(),this.getHeight(), null);
 
-		}else if(lastDir == -1){
-			g.drawImage(sprite_left, this.getX() - Camera.x,this.getY() - Camera.y,this.getWidth(),this.getHeight(), null);
-
-		}else if(lastDir_up_down == 1) {
-			g.drawImage(sprite_up, this.getX() - Camera.x,this.getY() - Camera.y,this.getWidth(),this.getHeight(), null);
-
-		}else {
-			g.drawImage(sprite_down, this.getX() - Camera.x,this.getY() - Camera.y,this.getWidth(),this.getHeight(), null);
+		}else if(moved == true && lastDir == -1){
+			g.drawImage(sprite_left[index], this.getX() - Camera.x,this.getY() - Camera.y,this.getWidth(),this.getHeight(), null);
 
 		}
-	}
-		
+	}		
 }
